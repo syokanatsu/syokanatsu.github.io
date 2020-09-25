d57d8e636417b0a2ba76d70c3f8ad4e8c65550f7
syokanatsu <330018177@qq.com>
2020-09-25 21:15:57 +0800

    megumi

--- js/main.js
+++ js/main.js
@@ -1,92 +1,40 @@
-$(document).ready(function(){
+(function () {
 
-toggle_nav_container();
-gotoByScroll();
+  var bv = new Bideo();
+  bv.init({
+    // Video element
+    videoEl: document.querySelector('#background_video'),
 
+    // Container element
+    container: document.querySelector('body'),
 
-});
+    // Resize
+    resize: true,
 
+    // autoplay: false,
 
+    isMobile: window.matchMedia('(max-width: 768px)').matches,
 
-var toggle_nav_container = function () {
+    playButton: document.querySelector('#play'),
+    pauseButton: document.querySelector('#pause'),
 
+    // Array of objects containing the src and type
+    // of different video formats to add
+    src: [
+      {
+        //src: '//media.html5media.info/video.mp4', //night.mp4  ////cloud.video.taobao.com/play/u/3245834217/p/1/e/6/t/1/50021244141.mp4
+        src: 'res/video/megumi_pc.mp4',
+        type: 'video/mp4'
+      },
+      {
+        src: 'night.webm',
+        type: 'video/webm;codecs="vp8, vorbis"'
+      }
+    ],
 
-
-	var 	$toggleButton = $('#toggle_m_nav');
-			$navContainer = $('#m_nav_container');
-			$menuButton = $('#m_nav_menu')
-			$menuButtonBars = $('.m_nav_ham');
-			$wrapper = $('#wrapper');
-
-	// toggle the container on click of button (can be remapped to $menuButton)
-
-	$toggleButton.on("click", function(){
-
-		// declare a local variable for the window width
-		var $viewportWidth = $(window).width();
-
-		// if statement to determine whether the nav container is already toggled or not
-
-		if($navContainer.is(':hidden'))
-		{	
-			$wrapper.removeClass('closed_wrapper');
-			$wrapper.addClass("open_wrapper");
-			$navContainer.slideDown(200).addClass('container_open').css("z-index", "2");
-			// $(window).scrollTop(0);
-			$menuButtonBars.removeClass('button_closed');
-			$menuButtonBars.addClass('button_open');
-			$("#m_ham_1").addClass("m_nav_ham_1_open");
-			$("#m_ham_2").addClass("m_nav_ham_2_open");
-			$("#m_ham_3").addClass("m_nav_ham_3_open");
-
-		}
-		else
-		{
-			$navContainer.css("z-index", "0").removeClass('container_open').slideUp(200)
-			$menuButtonBars.removeClass('button_open')
-			$menuButtonBars.addClass('button_closed')
-			$wrapper.removeClass('open_wrapper')
-			$wrapper.addClass("closed_wrapper")
-			$("#m_ham_1").removeClass("m_nav_ham_1_open");
-			$("#m_ham_2").removeClass("m_nav_ham_2_open");
-			$("#m_ham_3").removeClass("m_nav_ham_3_open");
-
-		}
-	});
-
-
-
-}
-
-
-// Function that takes the href value of links in the navbar and then scrolls 
-//the div on the page whose ID matches said value. This only works if you use 
-//a consistent naming scheme for the navbar anchors and div IDs
-
-var gotoByScroll = function (){
-
-	$(".m_nav_item a").on("click", function(e) {
-
-		e.preventDefault();
-		// var $divID =$(this).attr("href");
-		// var $scrollToDiv = "$(" + "'" + $divID + "'" +")";
-		
-		$('html,body').animate({
-   scrollTop: $($(this).attr("href")).offset().top - 50
-}, "slow");
-
-	});
-		
-
-
-
-}
-
-
-
-
-
-
-
-
-
+    // What to do once video loads (initial frame)
+    onLoad: function () {
+      document.querySelector('#video_cover').style.display = 'none';
+    }
+  });
+}());
